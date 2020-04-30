10a2fd7406e33d40ba62efa8584d025f4313ae19
JerOOme35 <chevert.jerome@orange.fr>
2020-04-30 08:58:32 +0200

    update

--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -2,12 +2,13 @@
 include 'Form.php';
 
 // Des variables par défaut pour vos tests.
-
+$lastname = 'Chevert';
+$firstname = 'Jerome';
 // YOUR CODE HERE
 
 $form = new Form($action, $method);  // créer le début du formulaire
-$form->addTextField('nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
-$form->addTextField('prenom',$prenom); // créer un input de type texte avec comme valeur par défaut $prenom
-$form->addSubmitButton('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form->addTextField('Chevert',$lastname); // créer un input de type texte avec comme valeur par défaut $nom
+$form->addTextField('Jerome',$firstname); // créer un input de type texte avec comme valeur par défaut $prenom
+$form->addSubmitButton('Modifier',$button); //Créer un bouton pour soumettre le formulaire se nommant Modifier
 
 echo $form->build(); // générer le formulaire
